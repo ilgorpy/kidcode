e332a2343149f1add6220dd498e7c3927ced0301
Ilya Gorbachev <rekil49118@gmail.com>
2024-12-04 00:35:42 +0400

    beta version filter

--- kidcode/mainapp/forms.py
+++ kidcode/mainapp/forms.py
@@ -4,4 +4,7 @@ from mainapp.models import Task, Grade, Code
 
 
 class JournalForm(forms.Form):
-    status = forms.ChoiceField(choices=get_user_model().GRADE_STATUS, label='Статус')
+    status = forms.ChoiceField(choices=Grade.STATUS_CHOICES, label="Статус", widget=forms.Select)
+    level = forms.ChoiceField(choices=[(t.level, t.level) for t in Task.objects.all().distinct('level')], label="Уровень", widget=forms.Select)
+    name = forms.ChoiceField(choices=[(g.user.id, g.user.name) for g in Grade.objects.all().distinct('user')], 
+                             label="Имя", widget=forms.Select)
